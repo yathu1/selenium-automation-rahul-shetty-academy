724e5ed4f62d5529e076854ea1c9f4c073612d70
yathu1 <yathu2198@gmail.com>
2025-06-22 20:41:10 +0530

    Applied Testng assertions

--- src/main/java/UpdatedDropDown.java
+++ src/main/java/UpdatedDropDown.java
@@ -1,15 +1,18 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class UpdatedDropDown {
 
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-        System.out.println(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
+        Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
+       // System.out.println(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
         driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).click();
-        System.out.println(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
+        Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
+      //  System.out.println(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
 
         //count the number of checkboxes
         System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
@@ -21,6 +24,7 @@ public class UpdatedDropDown {
             driver.findElement(By.id("hrefIncAdt")).click();
             i++;
         }
+        Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
         System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
         driver.findElement(By.id("btnclosepaxoption")).click();
     }
