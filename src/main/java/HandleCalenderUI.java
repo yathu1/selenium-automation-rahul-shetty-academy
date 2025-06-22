1ec0783f618ba194d984a2cb12cadfefadba3bae
yathu1 <yathu2198@gmail.com>
2025-06-22 23:19:02 +0530

    Validating if Ui elements are disabled or enabled with attributes

--- src/main/java/HandleCalenderUI.java
+++ src/main/java/HandleCalenderUI.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class HandleCalenderUI {
 
@@ -9,9 +10,18 @@ public class HandleCalenderUI {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 
-        driver.findElement(By.cssSelector(".ui-state-default.ui-state-active.ui-state-hover")).click();
+     //   driver.findElement(By.cssSelector(".ui-state-default.ui-state-active.ui-state-hover")).click();
 
+        driver.findElement(By.name("ctl00$mainContent$view_date2")).isEnabled();
+        driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
+        driver.findElement(By.name("ctl00$mainContent$view_date2")).isEnabled();
 
+        if(driver.findElement(By.id("Div1")).getDomAttribute("style").contains("1")){
+            System.out.println("its enabled");
+            Assert.assertTrue(true);
+        }else{
+            Assert.assertTrue(false);
+        }
 
     }
 }
